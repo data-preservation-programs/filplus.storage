3e2adfa234138ea7be7f5f1d3758b7f4e6c3b633
tikagan <30419893+tikagan@users.noreply.github.com>
2023-01-20 11:49:02 -0500

    feat: core functionality of wysiwyg form field abstracted to forms module

--- packages/fe/components/form/fields/wysiwyg.vue
+++ packages/fe/components/form/fields/wysiwyg.vue
@@ -1,84 +1,17 @@
 <template>
-  <div class="field field-wysiwyg">
+  <div :class="['field field-wysiwyg', state]">
 
-    <div v-if="editor" class="wysiwyg-container">
-      <div class="wysiwyg-formatting-container">
-        <Select
-          class="wysiwig-heading-dropdown"
-          :field="headingSelectField"
-          @updateValue="updateHeadingSelectNode" />
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive('bold') }]"
-          @clicked="editor.chain().focus().toggleBold().run()">
-          <strong>B</strong>
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive('italic') }]"
-          @clicked="editor.chain().focus().toggleItalic().run()">
-          <em>I</em>
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive('underline') }]"
-          @clicked="editor.chain().focus().toggleUnderline().run()">
-          <u>U</u>
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive('bulletList') }]"
-          @clicked="editor.chain().focus().toggleBulletList().run()">
-          bullet list
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive('orderedList') }]"
-          @clicked="editor.chain().focus().toggleOrderedList().run()">
-          ordered list
-        </ButtonX>
-        <ButtonX
-          class="wysiwyg-formatting-button"
-          @clicked="editor.chain().focus().undo().run()">
-          undo
-        </ButtonX>
-        <ButtonX
-          class="wysiwyg-formatting-button"
-          @clicked="editor.chain().focus().redo().run()">
-          redo
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive({ textAlign: 'left' }) }]"
-          @clicked="editor.chain().focus().setTextAlign('left').run()">
-          left
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive({ textAlign: 'center' }) }]"
-          @clicked="editor.chain().focus().setTextAlign('center').run()">
-          center
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive({ textAlign: 'right' }) }]"
-          @clicked="editor.chain().focus().setTextAlign('right').run()">
-          right
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive({ textAlign: 'justify' }) }]"
-          @clicked="editor.chain().focus().setTextAlign('justify').run()">
-          justify
-        </ButtonX>
-        <ButtonX
-          :class="[ 'wysiwyg-formatting-button', { 'is-active': editor.isActive('link') }]"
-          @clicked="setLink">
-          link
-        </ButtonX>
-      </div>
-      <client-only>
-        <editor-content
-          :id="fieldKey"
-          v-model="value"
-          :editor="editor"
-          :type="inputType"
-          :name="fieldKey"
-          :value="value"
-          :class="['wysiwyg-editor', state]" />
-      </client-only>
-    </div>
+    <Wysiwyg
+      :id="fieldKey"
+      :field="field"
+      :toolbar-config="toolbarConfig"
+      @updateEditorValue="updateEditorValue">
+
+      <template #toolbar-option-name="{ toolbarOption }">
+        <span v-html="toolbarOption.label ? toolbarOption.label : toolbarOption.name" />
+      </template>
+
+    </Wysiwyg>
 
   </div>
 
@@ -87,23 +20,15 @@
 <script>
 
 // ===================================================================== Imports
-import { Editor, EditorContent } from '@tiptap/vue-2'
-import StarterKit from '@tiptap/starter-kit'
-import TextAlign from '@tiptap/extension-text-align'
-import Link from '@tiptap/extension-link'
-import Underline from '@tiptap/extension-underline'
 
-import Select from '@/components/form/fields/select'
-import ButtonX from '@/components/buttons/button-x'
+import Wysiwyg from '@/modules/form/components/Wysiwyg'
 
 // ====================================================================== Export
 export default {
   name: 'FieldWysiwyg',
 
   components: {
-    EditorContent,
-    Select,
-    ButtonX
+    Wysiwyg
   },
 
   props: {
@@ -116,103 +41,111 @@ export default {
   data () {
     return {
       editor: null,
-      headingSelectValue: 0
+      defaultToolbarConfig: [
+        {
+          name: 'heading-select',
+          type: 'select',
+          include: true
+        },
+        {
+          name: 'bold',
+          label: '<b>B</b>',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'italic',
+          label: '<em>I</em>',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'underline',
+          label: '<u>U</u>',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'bulletList',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'orderedList',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'undo',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'redo',
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'left',
+          checkActive: { textAlign: 'left' },
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'center',
+          checkActive: { textAlign: 'center' },
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'right',
+          checkActive: { textAlign: 'right' },
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'justify',
+          checkActive: { textAlign: 'justify' },
+          type: 'button-x',
+          include: true
+        },
+        {
+          name: 'link',
+          type: 'button-x',
+          include: true
+        }
+      ]
     }
   },
 
   computed: {
-    inputType () {
-      return this.field.inputType || 'text'
-    },
-    fieldKey () {
-      return this.field.fieldKey
+    scaffold () {
+      return this.field.scaffold
     },
     value () {
       return this.field.value
     },
+    toolbarConfig () {
+      if (this.field.toolbarConfig !== undefined) {
+        return this.field.toolbarConfig
+      }
+      return this.defaultToolbarConfig
+    },
+    required () {
+      return this.field.required
+    },
+    fieldKey () {
+      return this.field.fieldKey
+    },
     state () {
       return this.field.state
-    },
-    headingSelectField () {
-      return {
-        id: `wysiwyg_heading_select|${this.field.id}`,
-        scaffold: this.field.scaffold.wysiwyg_heading_select,
-        value: this.headingSelectValue
-      }
     }
   },
 
-  mounted () {
-    this.editor = new Editor({
-      content: this.value,
-      extensions: [
-        StarterKit,
-        TextAlign.configure({
-          types: ['heading', 'paragraph']
-        }),
-        Link.configure({
-          HTMLAttributes: {
-              class: 'wysiwyg-link',
-              style: 'text-decoration: underline;'
-          },
-          openOnClick: false
-        }),
-        Underline
-      ],
-      onUpdate: () => {
-        this.$emit('updateValue', this.editor.getHTML())
-      },
-      onSelectionUpdate: ({ editor }) => {
-        const anchor = editor.state.selection.$anchor.pos
-        const head = editor.state.selection.$head.pos
-        const anchorNodeType = editor.state.selection.$anchor.parent.type.name === 'heading' ? editor.state.selection.$anchor.parent.attrs.level : editor.state.selection.$anchor.parent.type.name
-        const headNodeType = editor.state.selection.$head.parent.type.name === 'heading' ? editor.state.selection.$head.parent.attrs.level : editor.state.selection.$head.parent.type.name
-        if (anchor === head || anchorNodeType === headNodeType) {
-          this.showCurrentHeadingValue(anchorNodeType)
-        } else {
-          this.headingSelectValue = -1 // blanks dropdown if more than one node is selected, should blank if more than one heading type is selected
-        }
-      }
-    })
-  },
-
-  beforeDestroy () {
-    this.editor.destroy()
-  },
-
   methods: {
-    showCurrentHeadingValue (nodeType) {
-      switch (nodeType) {
-        case 1:
-          this.headingSelectValue = 1
-          break
-        case 2:
-          this.headingSelectValue = 2
-          break
-        default: // default sets it to paragraph/Normal text
-          this.headingSelectValue = 0
-      }
-    },
-    updateHeadingSelectNode (value) {
-      if (value === 0) {
-        this.headingSelectValue = 0 // set the heading option to be shown in the dropdown
-        this.editor.chain().focus().setParagraph().run() // change the node type that's currently in focus
-      }
-      this.headingSelectValue = value
-      this.editor.chain().focus().setHeading({ level: value }).run()
-    },
-    setLink () {
-      const previousURL = this.editor.getAttributes('link').href
-      const url = window.prompt('URL: ', previousURL)
-
-      if (url === null) {
-        return
-      }
-      if (url === '') {
-        this.editor.chain().focus().extendMarkRange('link').unsetLink().toggleUnderline().run()
-        return
-      }
-      this.editor.chain().focus().extendMarkRange('link').setLink({ href: url }).toggleUnderline().run()
+    updateEditorValue (value) {
+      this.editor = value
+      this.$emit('updateValue', value)
     }
   }
 }
@@ -230,33 +163,6 @@ export default {
   select {
     background: transparent;
   }
-}
-
-.wysiwyg-formatting-container {
-  display: flex;
-  flex-direction: row;
-}
-
-.wysiwig-heading-dropdown {
-  width: 8rem;
-}
-
-.wysiwyg-formatting-button {
-  margin-left: .25rem;
-  padding: 0 .2rem;
-  border: 2px solid $nandor;
-  border-radius: 0.625rem;
-  background-color: $racingGreen;
-  &.is-active {
-    border-color: $mandysPink;
-  }
-}
-
-.wysiwyg-editor {
-  padding: 1.5rem;
-  border: 2px solid $nandor;
-  border-radius: 0.625rem;
-  line-height: 1.1;
 }
 
 </style>
---
+++ packages/fe/modules/form/components/wysiwyg.vue
@@ -0,0 +1,250 @@
+<template>
+  <div v-if="editor" class="wysiwyg-container">
+
+    <!-- =============================================== Formatting Toolbar -->
+    <div class="wysiwyg-formatting-toolbar">
+      <div
+        v-for="formatTool in toolbarConfig"
+        :key="formatTool.name"
+        class="wysiwyg-formatting-option">
+        <Select
+          v-if="formatTool.type === 'select' && formatTool.include"
+          class="wysiwig-formatting-dropdown"
+          :field="headingSelectField"
+          @updateValue="updateNodeHeading" />
+        <ButtonX
+          v-if="formatTool.type === 'button-x' && formatTool.include"
+          :class="[ 'wysiwyg-formatting-button', formatTool.name, isFormatButtonActive(formatTool)]"
+          @clicked="clickFormatButton(formatTool)">
+          <slot name="toolbar-option-name" :toolbar-option="formatTool" />
+        </ButtonX>
+      </div>
+    </div>
+
+    <client-only>
+      <EditorContent
+        :editor="editor"
+        class="wysiwyg-editor" />
+    </client-only>
+
+  </div>
+</template>
+
+<script>
+// ===================================================================== Imports
+import { Editor, EditorContent } from '@tiptap/vue-2'
+import StarterKit from '@tiptap/starter-kit'
+import TextAlign from '@tiptap/extension-text-align'
+import Link from '@tiptap/extension-link'
+import Underline from '@tiptap/extension-underline'
+
+import ButtonX from '@/components/buttons/button-x'
+import Select from '@/components/form/fields/select'
+
+// ====================================================================== Export
+export default {
+  name: 'Wysiwyg',
+
+  components: {
+    EditorContent,
+    Select,
+    ButtonX
+  },
+
+  props: {
+    field: {
+      type: Object,
+      required: true
+    },
+    toolbarConfig: {
+      type: Array,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      editor: null,
+      headingSelectValue: 0
+    }
+  },
+
+  computed: {
+    value () {
+      return this.field.value
+    },
+    state () {
+      return this.field.state
+    },
+    headingSelectField () {
+      return {
+        id: `wysiwyg_heading_select|${this.field.id}`,
+        scaffold: {
+          type: 'select',
+          modelKey: 'wysiwyg_heading_select',
+          label: '',
+          required: true,
+          output: 'option',
+          react: {
+            fieldKey: 'wysiwyg_heading_select',
+            func: '$selectOption',
+            args: {
+              value_from_field: 'wysiwyg_heading_select'
+            }
+          },
+          options: [
+            { label: 'Normal text' },
+            { label: 'Heading 1' },
+            { label: 'Heading 2' }
+          ],
+          defaultValue: 0
+        },
+        value: this.headingSelectValue
+      }
+    }
+  },
+
+  mounted () {
+    this.editor = new Editor({
+      content: this.value,
+      extensions: [
+        StarterKit,
+        TextAlign.configure({
+          types: ['heading', 'paragraph']
+        }),
+        Link.configure({
+          HTMLAttributes: {
+            class: 'wysiwyg-link'
+          },
+          openOnClick: false
+        }),
+        Underline
+      ],
+      onUpdate: () => {
+        const value = this.editor.getHTML()
+        this.$emit('updateEditorValue', value)
+      },
+      onSelectionUpdate: ({ editor }) => {
+        const anchor = editor.state.selection.$anchor.pos
+        const head = editor.state.selection.$head.pos
+        const anchorNodeType = editor.state.selection.$anchor.parent.type.name === 'heading' ? editor.state.selection.$anchor.parent.attrs.level : editor.state.selection.$anchor.parent.type.name
+        const headNodeType = editor.state.selection.$head.parent.type.name === 'heading' ? editor.state.selection.$head.parent.attrs.level : editor.state.selection.$head.parent.type.name
+        if (anchor === head || anchorNodeType === headNodeType) {
+          this.showCurrentHeadingValue(anchorNodeType)
+        } else {
+          this.headingSelectValue = -1
+        }
+      }
+    })
+  },
+
+  beforeDestroy () {
+    this.editor.destroy()
+  },
+
+  methods: {
+    showCurrentHeadingValue (nodeType) {
+      switch (nodeType) {
+        case 1:
+          this.headingSelectValue = 1
+          break
+        case 2:
+          this.headingSelectValue = 2
+          break
+        default: // default sets it to paragraph/Normal text
+          this.headingSelectValue = 0
+      }
+    },
+    updateNodeHeading (value) {
+      if (value === 0) {
+        this.editor.chain().focus().setParagraph().run() // change the node type that's currently in focus
+        this.headingSelectValue = 0 // set the heading option to be shown in the dropdown
+      }
+      this.editor.chain().focus().setHeading({ level: value }).run()
+      this.headingSelectValue = value
+    },
+    setLink () {
+      const previousURL = this.editor.getAttributes('link').href
+      const url = window.prompt('URL: ', previousURL)
+
+      if (url === null) {
+        return
+      }
+      if (url === '') {
+        this.editor.chain().focus().extendMarkRange('link').unsetLink().toggleUnderline().run()
+        return
+      }
+      this.editor.chain().focus().extendMarkRange('link').setLink({ href: url }).toggleUnderline().run()
+    },
+    isFormatButtonActive (formatTool) {
+      if (formatTool.checkActive !== undefined) {
+        return this.editor.isActive(formatTool.checkActive) ? 'is-active' : ''
+      }
+      return this.editor.isActive(formatTool.name) ? 'is-active' : ''
+    },
+    clickFormatButton (formatTool) {
+      if (formatTool.checkActive !== undefined) {
+        this.editor.chain().focus().setTextAlign(formatTool.name).run()
+      }
+      switch (formatTool.name) {
+        case 'bold':
+          this.editor.chain().focus().toggleBold().run()
+          break
+        case 'italic':
+          this.editor.chain().focus().toggleItalic().run()
+          break
+        case 'underline':
+          this.editor.chain().focus().toggleUnderline().run()
+          break
+        case 'bulletList':
+          this.editor.chain().focus().toggleBulletList().run()
+          break
+        case 'orderedList':
+          this.editor.chain().focus().toggleOrderedList().run()
+          break
+        case 'undo':
+          this.editor.chain().focus().undo().run()
+          break
+        case 'redo':
+          this.editor.chain().focus().redo().run()
+          break
+        case 'link':
+          this.setLink()
+          break
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// ///////////////////////////////////////////////////////////////////// General
+.wysiwyg-formatting-toolbar {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1rem;
+}
+
+.wysiwig-formatting-dropdown {
+  width: 8rem;
+}
+
+.wysiwyg-formatting-button {
+  margin-left: .25rem;
+  padding: 0 .2rem;
+  border: 2px solid $nandor;
+  border-radius: 0.625rem;
+  background-color: $racingGreen;
+  &.is-active {
+    border-color: $mandysPink;
+  }
+}
+
+.wysiwyg-editor {
+  padding: 1.5rem;
+  border: 2px solid $nandor;
+  border-radius: 0.625rem;
+  line-height: 1.1;
+}
+
+</style>
