1ce0e4bdd0d9a69efe76f810b5587de618f27c17
Nauras Jabari <nauras@agencyundone.com>
2023-01-30 09:58:43 -0500

    feat: updated $gtm event names for /apply page and added two new $gtm events

--- packages/fe/plugins/helpers.js
+++ packages/fe/plugins/helpers.js
@@ -460,12 +460,14 @@ const HandleFormRedirection = (app, store, bytes, bottom, top) => {
       'https://verify.glif.io/',
       '_blank'
     )
+    this.$gtm.push({ event: 'redirect_verifyGlif' })
   } else if (bytes > top) {
     app.$toaster.add({
       type: 'toast',
       category: 'error',
       message: 'Please select a value up to 5 PiB'
     })
+    this.$gtm.push({ event: 'capture_greaterThanFivePibRequested' })
   } else {
     return true
   }
