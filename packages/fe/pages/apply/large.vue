1d5653474d26582d1ad9d2926ccf32bbe146cdca
tikagan <30419893+tikagan@users.noreply.github.com>
2023-02-01 10:23:54 -0500

    feat: LDA back button

--- packages/fe/pages/apply/large.vue
+++ packages/fe/pages/apply/large.vue
@@ -5,7 +5,8 @@
     <HeroB
       :label="hero.label"
       :heading="heroHeading"
-      :subtext="hero.subtext" />
+      :subtext="hero.subtext"
+      :hero-button="backButton" />
 
     <!-- ============================== [Application] Background Information -->
     <div id="application-top">
@@ -249,27 +250,25 @@
             field-key="confirm_follow_fil_guideline"
             form-id="filplus_application" />
 
-          <div v-if="account" class="buttons">
-            <ButtonA
-              class="submit-button"
-              loader="lda-submit-button"
-              @clicked="submitForm">
-              {{ submitButtonText }}
-            </ButtonA>
-            <ButtonA
-              v-if="githubIssueLink"
-              class="github-issue-link-button"
-              theme="blue"
-              tag="a"
-              target="_blank"
-              :to="githubIssueLink">
-              <GithubIcon />
-              {{ githubIssueLinkText }}
-            </ButtonA>
+          <div class="buttons">
+            <div v-if="account">
+              <ButtonA
+                class="submit-button"
+                loader="lda-submit-button"
+                @clicked="submitForm">
+                {{ submitButtonText }}
+              </ButtonA>
+            </div>
+
+            <AuthButton v-else />
+
+            <ButtonX
+              :to="backButton.href"
+              :tag="backButton.type">
+              {{ backButton.label }}
+            </ButtonX>
+
           </div>
-
-          <AuthButton v-else />
-
         </div>
       </div>
 
@@ -288,11 +287,10 @@ import { mapGetters, mapActions } from 'vuex'
 import HeroB from '@/components/hero-b'
 import FieldContainer from '@/components/form/field-container'
 import ButtonA from '@/components/buttons/button-a'
+import ButtonX from '@/components/buttons/button-x'
 import Overlay from '@/components/overlay'
 import Squigglie from '@/components/squigglie'
 import AuthButton from '@/components/auth-button'
-
-import GithubIcon from '@/components/icons/github'
 
 import ApplyLargePageData from '@/content/pages/apply-large.json'
 
@@ -304,10 +302,10 @@ export default {
     HeroB,
     FieldContainer,
     ButtonA,
+    ButtonX,
     Overlay,
     Squigglie,
-    AuthButton,
-    GithubIcon
+    AuthButton
   },
 
   meta: {
@@ -349,6 +347,9 @@ export default {
     },
     heroHeading () {
       return this.hero.heading.replace('|data|', this.networkStorageCapacity)
+    },
+    backButton () {
+      return this.pageData.back_button
     },
     form () {
       return this.pageData.form
@@ -456,10 +457,8 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
-  .button-a {
-    &:not(:last-child) {
-      margin-right: 1rem;
-    }
+  .button-x {
+    margin-left: 1rem;
   }
 }
 
