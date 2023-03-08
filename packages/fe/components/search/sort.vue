5553ebe837fb341cd978bd200a4bf296a186e7c1
Nauras Jabari <nauras@agencyundone.com>
2023-03-08 10:02:05 -0600

    feat: ported over new search module and related checkbox/limit/sort/pagination components

---
+++ packages/fe/components/search/sort.vue
@@ -0,0 +1,70 @@
+<template>
+  <Filterer
+    v-slot="{ applyFilter, originalSelected }"
+    filter-key="sort"
+    :is-single-option="true"
+    :options="options"
+    class="datasets-sort"
+    v-on="$listeners">
+    <FieldContainer
+      field-key="sort_by"
+      :scaffold="{
+        type: 'select',
+        required: false,
+        label: 'Sort by',
+        options,
+        defaultValue: originalSelected.length > 0 ? originalSelected : [0], /* manually set to 0 because default in store corresponds with the 0'th value in options prop */
+        resetGroupId: 'sort',
+        updateGroupId: 'sort',
+        isSingleOption: true
+      }"
+      @updateValue="initializeFilter($event, applyFilter)" />
+  </Filterer>
+</template>
+
+<script>
+// ===================================================================== Imports
+import Filterer from '@/modules/search/components/filterer'
+import FieldContainer from '@/components/form/field-container'
+
+// ====================================================================== Export
+export default {
+  name: 'Sort',
+
+  components: {
+    Filterer,
+    FieldContainer
+  },
+
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+
+  methods: {
+    async initializeFilter (index, applyFilter) {
+      await applyFilter({ index, live: false })
+      await this.$filter('page').for({ index: 0, live: false })
+      await this.$applyMultipleFiltersToQuery({ filters: ['page', 'sort'] })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// ///////////////////////////////////////////////////////////////////// General
+.datasets-sort {
+  margin-right: toRem(30);
+  @include medium {
+    margin-right: 0;
+  }
+  :deep(.field-label) {
+    white-space: nowrap;
+  }
+  :deep(.field-select) {
+    width: toRem(140);
+  }
+}
+</style>
--- packages/fe/modules/search/plugins/index.js
+++ packages/fe/modules/search/plugins/index.js
@@ -6,6 +6,8 @@
 
 // ///////////////////////////////////////////////////////// Imports & Variables
 // -----------------------------------------------------------------------------
+import CloneDeep from 'lodash/cloneDeep'
+
 import Store from '@/modules/search/store'
 
 // /////////////////////////////////////////////////////////////////// Functions
@@ -21,12 +23,83 @@ const registerStore = (store, next) => {
   if (next) { return next() }
 }
 
+// ///////////////////////////////////////////////////////////// resetFormFields
+const resetFormFields = async (items) => { // items = [{ id: '', resetTo: ''|undefined }]
+  const len = items.length
+  for (let i = 0; i < len; i++) {
+    const item = items[i]
+    /**
+      * This event is caught by the form module's field-standalone.vue component in its mounted() hook.
+      * @arg {object} payload
+      *   @param {string} id This id is passed to <FieldContainer> as a "resetGroupId" scaffold key. All fields with this id will be reset.
+      *   @param {string} resetTo 'nullState' or 'defaultValue'. Setting this will override the field-level resetTo value. DO NOT leave as an empty string.
+      */
+    window.$nuxt.$emit('resetFormFields', item)
+  }
+}
+
+// ///////////////////////////////////////////////// ApplyMultipleFiltersToQuery
+/**
+ * This function only works for query-based filters
+ */
+const ApplyMultipleFiltersToQuery = async (app, payload) => {
+  const filters = payload.filters
+  const redirect = payload.redirect
+  const query = CloneDeep(app.router.history.current.query)
+  const queryBefore = CloneDeep(query)
+  const len = filters.length
+  for (let i = 0; i < len; i++) {
+    const filterKey = filters[i]
+    const filter = await app.$filter(filterKey).get()
+    if (filter) {
+      const value = filter.queued
+      query[filterKey] = !value && query[filterKey] ? undefined : value
+    }
+  }
+  if (JSON.stringify(queryBefore) !== JSON.stringify(query)) {
+    app.router.push({
+      ...(redirect && { path: redirect }),
+      query,
+      hash: location.hash
+    })
+  }
+  await app.$delay(10)
+}
+
+// /////////////////////////////////////////////////////// ClearSearchAndFilters
+const ClearSearchAndFilters = async (app, payload) => {
+  try {
+    const searchers = payload.searchers || []
+    let filterers = payload.filters.clear || []
+    searchers.forEach(searcher => app.$search(searcher).clear())
+    filterers.forEach(filterer => app.$filter(filterer).clear())
+    filterers = filterers.concat(payload.filters.override || [])
+    app.$applyMultipleFiltersToQuery({ filters: filterers })
+    if (payload.resetFormFields) {
+      await resetFormFields(payload.resetFormFields)
+    }
+  } catch (e) {
+    console.log('========================== [Plugin: $clearSearchAndFilters]')
+    console.log(e)
+  }
+}
+
+// //////////////////////////////////////////////// CheckIfFilterSelectionsExist
+const CheckIfFilterSelectionsExist = async (app, filters) => {
+  let selelectionsExist = false
+  filters.forEach((filterKey) => {
+    if (!app.$filter(filterKey).isEmpty()) {
+      selelectionsExist = true
+    }
+  })
+  return selelectionsExist
+}
+
 // ////////////////////////////////////////////////////////////////////// Export
 // -----------------------------------------------------------------------------
-export default async function ({ app, store }, inject) {
+export default async function ({ app, store, route }, inject) {
   await registerStore(store)
-  inject('clearSearchAndFilters', () => {
-    app.$search.clearSearchQuery()
-    app.$filter.clearAll()
-  })
+  inject('applyMultipleFiltersToQuery', (filters) => ApplyMultipleFiltersToQuery(app, filters))
+  inject('clearSearchAndFilters', (filters) => ClearSearchAndFilters(app, filters))
+  inject('checkIfFilterSelectionsExist', (filters) => CheckIfFilterSelectionsExist(app, filters))
 }
