a940ed39ec411a2dad945584daf0d5b0fe64c7f0
svvimming <benmunozj@gmail.com>
2023-04-04 09:03:45 -0400

    feat: add animation params as props to warped grid component

--- packages/fe/components/warped-grid.vue
+++ packages/fe/components/warped-grid.vue
@@ -37,31 +37,30 @@ const renderGridOnCanvas = (instance, frame) => {
   const unit = params.cellWidth + params.lineWidth
   const width = unit * (vertical + 1)
   const height = unit * (horizontal + 1)
-  // calculation constants
+  const a = params.interactionIntensity
+  const b = params.pulseIntensity
+  // calculation variables
   const sv = vertical * params.resolution
   const sh = horizontal * params.resolution
   const hw = width / 2
   const z = frame * speed * Math.PI
-  const dv = 1 // displacement direction (x)
-  const du = 1 // displacement direction (y)
-  const dx = dv * Math.cos(z) * 100 // displacement magnitude (x)
-  const dy = du * Math.cos(z + speed * Math.sin(z)) * 100 // displacement magnitude (y)
-  const bv = 1 // buldge direction (x)
-  const bu = 1 // buldge direction (y)
+  const vec = instance.vectors
+  const dv = -1 * a * vec.aX + b * Math.cos(z)
+  const du = -1 * a * vec.aY + b * Math.sin(z + speed * Math.sin(z))
 
   ctx.clearRect(0, 0, width, height)
   ctx.lineWidth = params.lineWidth
-  ctx.strokeStyle = '#4D655F'
+  ctx.strokeStyle = params.color
 
   for (let i = 1; i <= vertical; i++) {
     ctx.beginPath()
     const x = i * unit
-    let g = gaussianSmoothStep(x + dx, 0 + dy, hw)
-    ctx.moveTo(x - g * bv, 0 - g * bu)
+    let g = gaussianSmoothStep(x + dv, 0 + du, hw)
+    ctx.moveTo(x - g * dv, 0 - g * du)
     for (let j = 1; j <= sh; j++) {
       const y = (j / sh) * height
-      g = gaussianSmoothStep(x + dx, y + dy, hw)
-      ctx.lineTo(x - g * bv, y - g * bu)
+      g = gaussianSmoothStep(x + dv, y + du, hw)
+      ctx.lineTo(x - g * dv, y - g * du)
     }
     ctx.stroke()
   }
@@ -69,15 +68,44 @@ const renderGridOnCanvas = (instance, frame) => {
   for (let i = 1; i <= horizontal; i++) {
     ctx.beginPath()
     const y = i * unit
-    let g = gaussianSmoothStep(0 + dx, y + dy, hw)
-    ctx.moveTo(0 - g * bv, y - g * bu)
+    let g = gaussianSmoothStep(0 + dv, y + du, hw)
+    ctx.moveTo(0 - g * dv, y - g * du)
     for (let j = 1; j <= sv; j++) {
       const x = (j / sv) * width
-      g = gaussianSmoothStep(x + dx, y + dy, hw)
-      ctx.lineTo(x - g * bv, y - g * bu)
+      g = gaussianSmoothStep(x + dv, y + du, hw)
+      ctx.lineTo(x - g * dv, y - g * du)
     }
     ctx.stroke()
   }
+
+  updateVectors(instance)
+}
+
+const updateMousePosition = (e, instance) => {
+  const vec = instance.vectors
+  const rect = instance.$refs.canvas.getBoundingClientRect()
+  // calculate mouse position
+  vec.x = (e.pageX - rect.left - window.scrollX) / rect.width
+  vec.y = (e.pageY - rect.top - window.scrollY) / rect.height
+}
+
+const updateVectors = (instance) => {
+  const vec = instance.vectors
+  const params = instance.params
+  // calculate displacement from mouse
+  vec.dx = vec.x - vec.cgx
+  vec.dy = vec.y - vec.cgy
+  // create springing effect
+  vec.dx *= params.springing
+  vec.dy *= params.springing
+  vec.aX += vec.dx
+  vec.aY += vec.dy
+  // move center
+  vec.cgx += vec.aX
+  vec.cgy += vec.aY
+  // slow down springing
+  vec.aX *= params.damping
+  vec.aY *= params.damping
 }
 
 // ====================================================================== Export
@@ -89,21 +117,92 @@ export default {
       type: Boolean,
       required: false,
       default: true
+    },
+    verticalLines: {
+      type: Number,
+      required: false,
+      default: 32
+    },
+    horizontalLines: {
+      type: Number,
+      required: false,
+      default: 32
+    },
+    cellWidth: {
+      type: Number,
+      required: false,
+      default: 33
+    },
+    lineWidth: {
+      type: Number,
+      required: false,
+      default: 1
+    },
+    resolution: { // number of sublines used to create each side of each cell
+      type: Number,
+      required: false,
+      default: 4
+    },
+    pulseSpeed: {
+      type: Number,
+      required: false,
+      default: 0.0015
+    },
+    pulseIntensity: {
+      type: Number,
+      required: false,
+      default: 0.5
+    },
+    interactionSpringing: {
+      type: Number,
+      required: false,
+      default: 0.001
+    },
+    interactionDamping: { // values above 1.0 produce divergence
+      type: Number,
+      required: false,
+      default: 0.97
+    },
+    interactionIntensity: {
+      type: Number,
+      required: false,
+      default: 75
+    },
+    gridColor: {
+      type: String,
+      required: false,
+      default: '#4D655F'
     }
   },
 
   data () {
+    const params = {
+      verticalLines: this.verticalLines,
+      horizontalLines: this.horizontalLines,
+      cellWidth: this.cellWidth,
+      lineWidth: this.lineWidth,
+      resolution: this.resolution,
+      speed: this.pulseSpeed,
+      pulseIntensity: this.pulseIntensity,
+      springing: this.interactionSpringing,
+      damping: this.interactionDamping,
+      interactionIntensity: this.interactionIntensity,
+      color: this.gridColor
+    }
     return {
+      params,
       ctx: false,
       frame: 0,
       frameId: false,
-      params: {
-        verticalLines: 32,
-        horizontalLines: 32,
-        cellWidth: 33,
-        lineWidth: 1,
-        resolution: 4,
-        speed: 0.0015
+      vectors: {
+        x: 0,
+        y: 0,
+        cgx: 0,
+        cgy: 0,
+        aX: 0,
+        aY: 0,
+        dx: 0,
+        dy: 0
       }
     }
   },
@@ -121,6 +220,10 @@ export default {
 
   mounted () {
     this.$nextTick(() => {
+      const canvas = this.$refs.canvas
+      if (canvas) {
+        canvas.addEventListener('mousemove', (e) => { updateMousePosition(e, this) })
+      }
       initCanvas(this, () => {
         renderGridOnCanvas(this, 0)
         this.animate()
