4ad6c7b1d60b07dec59b1464f433d1c5f2841a0f
tikagan <30419893+tikagan@users.noreply.github.com>
2023-06-21 13:40:23 -0400

    feat: application-stats cron adds efil+ and filplus.storage counts to map

--- packages/be/crons/application-stats.js
+++ packages/be/crons/application-stats.js
@@ -45,7 +45,7 @@ const { SecondsToHms } = require('@Module_Utilities')
 // -----------------------------------------------------------------------------
 // ////////////////////////////////////////////////////////////// getTotalIssues
 const getTotalIssues = async (repo, dataProgramsToken) => {
-  console.log('🦞 > 🦀 getting total issues')
+  // console.log('🦞 > 🦀 getting total issues')
   try {
     const options = {
       headers: { Accept: 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28', Authorization: `Bearer ${dataProgramsToken}` }
@@ -66,7 +66,7 @@ const getApplications = async (applicationType, n = 1, applications = [], totalI
   try {
     const dataProgramsToken = process.env.GITHUB__PERSONAL_ACCESS_TOKEN__DATA_PROGRAMS
     const repo = MC.repos[applicationType][0]
-    if (totalIssues === 0) { totalIssues = await getTotalIssues(repo, dataProgramsToken); console.log('totalIssues ', totalIssues) }
+    if (totalIssues === 0) { totalIssues = await getTotalIssues(repo, dataProgramsToken) }
 
     const options = {
       headers: { Accept: 'application/vnd.github+json', 'X-GitHub-Api-Version': '2022-11-28', Authorization: `Bearer ${dataProgramsToken}` },
@@ -76,10 +76,9 @@ const getApplications = async (applicationType, n = 1, applications = [], totalI
         page: n
       }
     }
-    const response = await Axios.get(`https://api.github.com/repos/${repo}/issues?q=is:issue`, options)
+    const response = await Axios.get(`https://api.github.com/repos/${repo}/issues`, options)
     const updatedApplications = [...applications, ...response.data]
     if (updatedApplications.length < totalIssues) {
-      console.log('updatedApplications.length totalIssues ', updatedApplications.length, totalIssues)
       return getApplications(applicationType, n + 1, updatedApplications, totalIssues)
     }
     return updatedApplications
@@ -90,34 +89,40 @@ const getApplications = async (applicationType, n = 1, applications = [], totalI
   }
 }
 
-// //////////////////////////////////////////////////////////////////// hasLabel
-const hasLabel = (labels, regex) => {
-  return labels.some((label) => { return label.name.match(regex) })
-}
-
-// //////////////////////////////////////////// determineApplicationStateByLabel
-const determineApplicationStateByLabel = (application, applicationType) => {
-  console.log('🦊 > 🐡 determining application state')
+// //////////////////////////////////////////// iterateApplicationLabels
+const iterateApplicationLabels = (application, applicationType) => {
+  // console.log('🦊 > 🐡 determining application state')
   try {
     const labels = application.labels
     const completedRegex = { ga: /(state:)?\s?granted/gi, lda: /total\s?datacap\s?reached/gi }
     const validatedRegex = { ga: /(bot:)?\s?looking\s?good/gi, lda: /validated/ }
     const inReviewRegex = { ga: /(state)|(bot)?:\s?(further\s?info)?(review)?\s?needed/gi, lda: /error/ }
 
+    const hasLabel = labels.some((label, regex) => { return label.name.match(regex) })
+
     const completed = hasLabel(labels, completedRegex[applicationType])
     const validated = hasLabel(labels, validatedRegex[applicationType])
     const inReview = hasLabel(labels, inReviewRegex[applicationType])
-    const Stale = hasLabel(labels, /Stale/)
     const isPR = application.pull_request
 
-    if (isPR) { return 'isPR' }
-    if (completed) { return 'completed' }
-    if (validated) { return 'validated' }
-    if (inReview) { return 'inReview' }
-    if (Stale) { return 'Stale' }
-    return false
+    const applicationState = isPR ? false
+      : completed ? 'completed'
+        : validated ? 'validated'
+          : inReview ? 'inReview'
+            : 'noRelevantLabels'
+
+    const filPlusStorageRegex = /(source:)?\s?filplus\.?storage/ig
+    const filPlusStorage = hasLabel(labels, filPlusStorageRegex)
+
+    if (applicationType === 'lda') {
+      const eFilPlusRegex = /e[-\s]?fil\s?(plus)?\+?/ig
+      const eFilPlus = hasLabel(labels, eFilPlusRegex)
+      return { applicationState, filPlusStorage, eFilPlus }
+    }
+    const eFilPlus = false
+    return { applicationState, filPlusStorage, eFilPlus }
   } catch (e) {
-    console.log('================ [Function: determineApplicationStateByLabel]')
+    console.log('================ [Function: iterateApplicationLabels]')
     console.log(e)
     throw e
   }
@@ -131,20 +136,42 @@ const iterateApplications = (applications, applicationType) => {
       completed: 0,
       validated: 0,
       inReview: 0,
-      Stale: 0,
-      isPR: 0,
       noRelevantLabels: []
     }
+    const types = {
+      lda: {
+        open: 0,
+        closed: 0
+      },
+      ga: {
+        open: 0,
+        closed: 0
+      },
+      eFilPlus: {
+        open: 0,
+        closed: 0
+      },
+      filPlusStorage: {
+        open: 0,
+        closed: 0
+      }
+
+    }
     applications.forEach((application) => {
-      const applicationState = determineApplicationStateByLabel(application, applicationType)
-      if (!applicationState) {
-        states.noRelevantLabels.push(application.number)
-      } else {
+      const { applicationState, filPlusStorage, eFilPlus } = iterateApplicationLabels(application, applicationType)
+      // if (applicationState === 'noRelevantLabels') {
+      //   states.noRelevantLabels.push(application.number)
+      // }
+      if (applicationState && applicationState !== 'noRelevantLabels') {
         states[applicationState]++
+        const issueState = application.state
+        types[applicationType][issueState]++
+
+        if (filPlusStorage) { types.filPlusStorage[issueState]++ }
+        if (eFilPlus) { types.eFilPlus[issueState]++ }
       }
     })
-    const stats = { states }
-    return stats
+    return { states, types }
   } catch (e) {
     console.log('============================= [Function: iterateApplications]')
     console.log(e)
@@ -158,20 +185,17 @@ const ApplicationStats = async () => {
   console.log('🤖 Fetch application stats started')
   try {
     const start = process.hrtime()[0]
+
     const gaApplications = await getApplications('ga')
     const ldaApplications = await getApplications('lda')
 
     const gaStats = await iterateApplications(gaApplications, 'ga')
     const ldaStats = await iterateApplications(ldaApplications, 'lda')
 
-    console.log('gaStats ', gaStats)
-    console.log('gaStats.states.noRelevantLabels.length ', gaStats.states.noRelevantLabels.length)
-    console.log('ldaStats ', ldaStats)
-    console.log('ldaStats.states.noRelevantLabels.length ', ldaStats.states.noRelevantLabels.length)
-
     // if (data) {
     //   Fs.writeFileSync(`${MC.cacheRoot}/application-stats.json`, JSON.stringify(data))
     // }
+
     const end = process.hrtime()[0]
     console.log(`🏁 Fetch network storage capacity complete | took ${SecondsToHms(end - start)}`)
     process.exit(0)
